ecfaf8b7532c3353f0d2158dea7849216bfa3258
Morris sardo <zlac176@live.rhul.ac.uk>
2024-02-01 11:35:30 +0000

    Create appication package for src/main and src/resurces/main.

--- restaurant/src/main/java/appication/ConnectionToDB.java
+++ restaurant/src/main/java/appication/ConnectionToDB.java
@@ -1,3 +1,5 @@
+package appication;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
--- restaurant/src/main/java/appication/Driver.java
+++ restaurant/src/main/java/appication/Driver.java
@@ -1,3 +1,5 @@
+package appication;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
