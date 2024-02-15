9ff9deec421ee859c7bdb0ba79e69b4566934937
Xavier Noel <ZLAC165@live.rhul.ac.uk>
2024-02-15 16:17:08 +0000

    fixed missing predicate import

--- restaurant/src/main/java/backend/Waiter.java
+++ restaurant/src/main/java/backend/Waiter.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.function.Predicate;
 import org.postgresql.util.PSQLException;
 
 /**
