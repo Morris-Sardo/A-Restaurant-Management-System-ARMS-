71950052e47d3e0f8bb4b0859def80fb3bdfb673
Xavier Noel <ZLAC165@live.rhul.ac.uk>
2024-02-15 14:55:33 +0000

    added viewOrders method

--- restaurant/src/main/java/backend/Waiter.java
+++ restaurant/src/main/java/backend/Waiter.java
@@ -9,19 +9,21 @@ import org.postgresql.util.PSQLException;
 
 /**
  * Contains the methods needed for the 'Customer' role.
+ * 
  * @author xaviernoel
  */
 public class Waiter {
   int waiterID;
   Connection connection = null;
-  
+
   public Waiter(int waiterID, Connection connection) {
     this.waiterID = waiterID;
     this.connection = connection;
   }
-  
+
   /**
    * Finds all items on the menu.
+   * 
    * @return the IDs of all items on the menu
    */
   public ArrayList<Integer> viewMenu()
@@ -39,84 +41,106 @@ public class Waiter {
     }
     return results;
   }
-  
+
   /**
    * Toggles the availability of the item.
+   * 
    * @param item the item to be changed
    */
   public void changeMenu(int item) {
-    
+
   }
-  
+
   /**
    * Finds all orders with 'Requested' status.
+   * 
    * @return the list of the information for orders
    */
-  public String[] viewOrders() {
-    return null;
+  // Temporary, final version will need an agreement on how data should be communicated.
+  public ArrayList<String> viewOrders()
+      throws PSQLException, SQLException, DatabaseInformationException {
+    ArrayList<String> results = new ArrayList<String>();
+    String query = "SELECT * FROM orders WHERE status == 'Requested' ";
+    try (PreparedStatement statement = connection.prepareStatement(query);) {
+      ResultSet resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        results.add(
+            "Order Number: " + resultSet.getString(1) + ", Table Number: " + resultSet.getString(2)
+                + ", Items: " + resultSet.getString(3) + ", Order Time: " + resultSet.getString(5));
+      }
+    }
+    return results;
   }
-  
+
   /**
    * Changes the status of the order to 'Confirmed'.
+   * 
    * @param orderNumber the number of the order to be changed
    */
   public void confirmOrder(int orderNumber) {
-    
+
   }
-  
+
   /**
    * Changes the status of the order to 'Delivered'.
+   * 
    * @param orderNumber the number of the order to be changed
    */
   public void deliveredOrder(int orderNumber) {
-    
+
   }
-  
+
   /**
    * Changes the status of this order to 'Canceled'.
+   * 
    * @param orderNumber the number of the order to be changed
    */
   public void cancelOrder(int orderNumber) {
-    
+
   }
-  
+
   /**
    * Finds all orders with 'Ready' status.
+   * 
    * @return the list of the information for orders
    */
   public ArrayList<String> readyOrders() {
     return null;
   }
-  
+
   /**
    * Finds all bill requests.
+   * 
    * @return the list of the information for bill requests
    */
   public ArrayList<String> viewBillRequests() {
     return null;
   }
-  
+
   /**
    * Finds all complaints.
+   * 
    * @return the list of table numbers with a current complaint
    */
   public ArrayList<Integer> viewComplaints() {
     return null;
   }
-  
+
   /**
    * Sets the status of a bill to 'Completed'.
+   * 
    * @param tableNumber the table number of the bill to be changed
    */
   public void concludeBill(int tableNumber) {
-    
+
   }
-  
+
   /**
    * Sets the status of a complaint to 'Completed'.
+   * 
    * @param tableNumber the table number of the complaint to be changed
    */
   public void concludeComplaint(int tableNumber) {
-    
+
   }
 }
