dc3e0460fae8efc49913fbf2a896f90bf5a602dd
Morris sardo <zlac176@live.rhul.ac.uk>
2024-02-29 14:03:54 +0000

    Create Inventory class and InventoryModel class. Those classes will implement the table property and the interaction between database and model

---
+++ restaurant/src/main/java/appication/Inventory.java
@@ -0,0 +1,11 @@
+package appication;
+
+/**
+ * This class will impleemtn all the property methos for the table inventory.
+ * 
+ * @author papap
+ *
+ */
+public class Inventory {
+
+}
---
+++ restaurant/src/main/java/appication/InventoryModel.java
@@ -0,0 +1,11 @@
+package appication;
+
+/**
+ * This is the interaction with database.
+ * 
+ * @author papap
+ *
+ */
+public class InventoryModel {
+
+}
