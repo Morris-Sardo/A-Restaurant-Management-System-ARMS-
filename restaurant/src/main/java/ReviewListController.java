d49618649223eacecdc5befc8ce8de0044cd2f1c
Zain Akhtar <ZLAC321@live.rhul.ac.uk>
2024-03-06 15:15:02 +0000

    Updated layout for ReviewList

--- restaurant/src/main/java/ReviewListController.java
+++ restaurant/src/main/java/ReviewListController.java
@@ -1,4 +1,4 @@
 
 public class ReviewListController {
-
+  // FAKE CODE
 }
--- restaurant/src/main/java/ReviewListView.java
+++ restaurant/src/main/java/ReviewListView.java
@@ -1,4 +1,22 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class ReviewListView {
+public class ReviewListView extends Application{
 
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+  // Fake Code
 }
