8a2743ab3e9b3449965224c3b1cb7882812c6a42
jnthnmrtn04 <zlac181@live.rhul.ac.uk>
2024-03-13 15:44:52 +0000

    Kitchen Orders assign properties

--- restaurant/src/main/java/appication/KitchenOrder.java
+++ restaurant/src/main/java/appication/KitchenOrder.java
@@ -2,6 +2,7 @@ package appication;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class KitchenOrder {
@@ -16,7 +17,7 @@ public class KitchenOrder {
   /**
    * This method sets the values orderID in orderID_Property.
    * 
-   * @param value id.
+   * @param value orderID.
    */
   public void setorderID(Integer value) {
     orderID_().set(value);
@@ -25,7 +26,7 @@ public class KitchenOrder {
   /**
    * This method gets the value stored into the orderID_Property.
    * 
-   * @return id.
+   * @return orderID.
    */
   public Integer getorderID() {
     return orderID_().get();
@@ -34,7 +35,7 @@ public class KitchenOrder {
   /**
    * This method instantiates a simpleintegerProperty for orderID.
    * 
-   * @return return id.
+   * @return return orderID.
    */
   public IntegerProperty orderID_() {
     if (orderID == null) {
@@ -47,25 +48,25 @@ public class KitchenOrder {
   /**
    * This method sets the values tableID in tableID_Property.
    * 
-   * @param value id.
+   * @param value tableID.
    */
-  public void settableID(Integer value) {
+  public void setTableID(Integer value) {
     tableID_().set(value);
   }
 
   /**
    * This method gets the value stored into the tableID_Property.
    * 
-   * @return id.
+   * @return tableID.
    */
-  public Integer gettableID() {
+  public Integer getTableID() {
     return tableID_().get();
   }
 
   /**
    * This method instantiates a simpleintegerProperty for tableID.
    * 
-   * @return return id.
+   * @return return tableID.
    */
   public IntegerProperty tableID_() {
     if (tableID == null) {
@@ -75,4 +76,129 @@ public class KitchenOrder {
 
   }
   
+  /**
+   * This method set items in items_Property.
+   * 
+   * @param value is the items string.
+   */
+  public void setItems(String value) {
+    itemsProperty().set(value);
+  }
+
+
+  /**
+   * This methods get items onto items_Proprety.
+   * 
+   * @return string name items.
+   */
+  public String getItems() {
+    return itemsProperty().get();
+  }
+
+  /**
+   * This methods instantiates the object items that has all the items properties.
+   * 
+   * @return property that holds the value of the items.
+   */
+  public StringProperty itemsProperty() {
+    if (items == null) {
+      items = new SimpleStringProperty(this, "items");
+    }
+    return items;
+
+  }
+
+  /**
+   * This method sets the values price in price_Property.
+   * 
+   * @param value price.
+   */
+  public void setPrice(Integer value) {
+    price_().set(value);
+  }
+
+  /**
+   * This method gets the value stored into the price_Property.
+   * 
+   * @return price.
+   */
+  public Integer getPrice() {
+    return price_().get();
+  }
+
+  /**
+   * This method instantiates a simpleintegerProperty for price.
+   * 
+   * @return return price.
+   */
+  public IntegerProperty price_() {
+    if (price == null) {
+      price = new SimpleIntegerProperty(this, "price");
+    }
+    return price;
+
+  }
+  
+  /**
+   * This method sets the values price in orderTime_Property.
+   * 
+   * @param value orderTime.
+   */
+  public void setOrderTime(Integer value) {
+    orderTime_().set(value);
+  }
+
+  /**
+   * This method gets the value stored into the orderTime_Property.
+   * 
+   * @return orderTime.
+   */
+  public Integer getOrderTime() {
+    return orderTime_().get();
+  }
+
+  /**
+   * This method instantiates a simpleintegerProperty for orderTime.
+   * 
+   * @return return orderTime.
+   */
+  public IntegerProperty orderTime_() {
+    if (orderTime == null) {
+      orderTime = new SimpleIntegerProperty(this, "orderTime");
+    }
+    return orderTime;
+
+  }
+  
+  /**
+   * This method set status in status_Property.
+   * 
+   * @param value is the status string.
+   */
+  public void setStatus(String value) {
+    statusProperty().set(value);
+  }
+
+
+  /**
+   * This methods get status onto status_Proprety.
+   * 
+   * @return string name status.
+   */
+  public String getStatus() {
+    return statusProperty().get();
+  }
+
+  /**
+   * This methods instantiates the object status that has all the status properties.
+   * 
+   * @return property that holds the value of the status.
+   */
+  public StringProperty status() {
+    if (status == null) {
+      status = new SimpleStringProperty(this, "status");
+    }
+    return status;
+
+  }
 }
