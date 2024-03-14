63f17edd91ca86604f8e7be8f7893a3852b1f043
Karol Zurawski <Karol.Zurawski.2022@live.rhul.ac.uk>
2024-03-14 17:50:02 +0000

    Added dummy data for database tables

--- database/PopulateDatabase.sql
+++ database/PopulateDatabase.sql
@@ -7,3 +7,24 @@ INSERT INTO tables VALUES(1, TRUE);
 INSERT INTO tables VALUES(2, TRUE);
 INSERT INTO tables VALUES(3, TRUE);
 INSERT INTO tables VALUES(4, FALSE);
+INSERT INTO tables VALUES(5, TRUE);
+
+INSERT INTO orders(order_number, customer_id, table_number, items, price, order_time, status) VALUES 
+    (1, 999, 1, [1, 3, 4], 10.97, '12:30', 'Pending'),
+    (2, 999, 3, [1, 2], 13.98, '12:30', 'Completed'),
+    (3, 999, 4, [2], 7.99, '12:30', 'Pending'),
+    (4, 999, 5, [4], 1.99, '12:30', 'Completed'),
+    (5, 999, 2, [1, 4], 7.98, '12:30', 'Pending');
+    
+INSERT INTO complaints (customer_id, table_number, status) VALUES
+    (999, 5, 'Great Service'),
+    (999, 4, 'Delayed Service');
+    
+INSERT INTO bills (customer_id, table_number, items, price, status) VALUES
+    (999, 1, [1, 3, 4], 10.97, 'Paid'),
+    (999, 3, [1, 2], 13.98, 'Unpaid'),
+    (999, 4, [2], 7.99,'Paid'),
+    (999, 5, [4], 1.99, 'Unpaid),
+    (999, 2, [1, 4], 7.98, 'Paid');
+
+    
