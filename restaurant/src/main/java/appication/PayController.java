3e767400781c8326469aa825edf0c4b84670a773
Morris sardo <zlac176@live.rhul.ac.uk>
2024-03-19 14:29:38 +0000

    Implemented paymentSubmission method. This method is used to verified if all the fields are filled. it pop up a series of alert.
    Impleemented prize method. This methtod is used to check is a table exist and set the prize bill on the field.

--- restaurant/src/main/java/appication/PayController.java
+++ restaurant/src/main/java/appication/PayController.java
@@ -1,5 +1,8 @@
 package appication;
 
+
+import javafx.scene.control.Alert.AlertType;
+
 /**
  * This class is used to initeract beteween view and model. This class will handle the exception as
  * well.
@@ -11,6 +14,11 @@ public class PayController {
 
   private PayView viewP;
 
+  /**
+   * This costrunctor used to initialize a view object class.
+   * 
+   * @param viewP is the object of pay view class.
+   */
   public PayController(PayView viewP) {
     this.viewP = viewP;
   }
@@ -25,6 +33,67 @@ public class PayController {
 
   }
 
+  /**
+   * This method handle to print the bills on the pay page.
+   */
+  public void handlePrize() {
+    if (viewP.getTableNumber() == -1) {
+      AlertText.alert(AlertType.ERROR, "Error Message", "Please Enter a valid number of table");
+    } else {
+      Float bills = PayModel.getPrizeFormTable(viewP.getTableNumber());
+      if (bills != null) {
+        viewP.setPrize(bills);
+      } else {
+        AlertText.alert(AlertType.ERROR, "Error Message", "Please Enter a valid number of table");
+
+      }
+    }
 
 
+  }
+
+  /**
+   * This method is used to submit a payment.
+   */
+  public void handlePaymentSubmission() {
+
+    String[] arr = viewP.getExpairedDate().split("/");
+    int count = 0;
+    try {
+      if (arr[0].length() == 2) {
+        count++;
+      }
+
+      if (arr[1].length() == 2) {
+        count++;
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      AlertText.alert(AlertType.ERROR, "Error Message", "Please check information in all fields");
+      return;
+    }
+
+
+    Float bills = PayModel.getPrizeFormTable(viewP.getTableNumber());
+
+    if (count != 2 || viewP.getTableNumber() == -1 || bills == null
+        || (viewP.getCCV() + "").length() != 3 || viewP.getCCV() == -1
+        || viewP.getExpairedDate().isEmpty() || viewP.getFirstName().isEmpty()
+        || viewP.getLastName().isEmpty() || (viewP.getNumberCard() + "").length() != 16
+        || viewP.getNumberCard() == -1) {
+      AlertText.alert(AlertType.ERROR, "Error Message", "Please check information in all fields");
+
+
+    } else {
+      if (PayModel.completeTransaction(viewP.getTableNumber())) {
+        AlertText.alert(AlertType.INFORMATION, "Successfull payement", "Successfully made payment");
+        viewP.handleCleanBillsFields();
+        viewP.handleClearFiled();
+
+      } else {
+        return;
+      }
+    }
+  }
 }
+
+
