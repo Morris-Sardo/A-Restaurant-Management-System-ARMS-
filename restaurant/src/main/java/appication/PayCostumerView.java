54b494a3245da1fc62567c529d94cd0b830a1bc6
Morris sardo <zlac176@live.rhul.ac.uk>
2024-03-21 13:46:45 +0000

    Added all the methods to clean the fields before and after nedded. Added method all the method to handle the mismatche imput. Added metods to handle the how to display ther prize, table and submission the payment. Create minimal database table such that the project could test all all the new features Created all the metod in the model class such chat. the database table is manipulate by data.

--- restaurant/src/main/java/appication/PayCostumerView.java
+++ restaurant/src/main/java/appication/PayCostumerView.java
@@ -1,12 +1,12 @@
 package appication;
 
-import java.io.IOException;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 /**
  * This class is used as interaction with the user.
@@ -15,71 +15,205 @@ import javafx.scene.control.Label;
  *
  */
 public class PayCostumerView {
-  // @FXML
-  // private TextField cardNumberField;
-  //
-  // @FXML
-  // private TextField ccvField;
-  //
-  // @FXML
-  // private Button clearFieldBtn;
-  //
-  // @FXML
-  // private TextField expairedField;
-  //
-  // @FXML
-  // private TextField firstNameField;
-  //
-  // @FXML
-  // private TextField lastNameField;
+
+
+  @FXML
+  private TextField tableNum;
+
+  @FXML
+  private TextField cardNumberField;
+
+  @FXML
+  private TextField ccvField;
+
+  @FXML
+  private Button clearFieldBtn;
+
+  @FXML
+  private TextField expairedField;
+
+  @FXML
+  private TextField firstNameField;
+
+  @FXML
+  private TextField lastNameField;
 
   @FXML
   private Button menuBtn;
-  //
-  // @FXML
-  // private ImageView outImage;
+
+  @FXML
+  private ImageView outImage;
 
   @FXML
   private Button signOutBtn;
 
-  // @FXML
-  // private Button submitPaymentBtn;
-  //
-  // @FXML
-  // private TextField totalBillToPayField;
-  //
-  // @FXML
-  // private Label username;
+  @FXML
+  private Button submitPaymentBtn;
+
+  @FXML
+  private TextField totalBillToPayField;
+
+  @FXML
+  private Label username;
+
+
+  private int tableNumber;
 
 
 
-  /**
-   * This method start the py page for costumer.
-   * 
-   * @return The pay costumer scene.
-   */
-  public Scene start() {
-    Parent root;
-    try {
-      root = FXMLLoader.load(getClass().getResource("payCostumerPage.fxml"));
-      Scene scene = new Scene(root, 1100, 600);
-      return scene;
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-    }
+  public PayCostumerView(int tableNumber) {
+    this.tableNumber = tableNumber;
   }
+
 
   /**
    * this isniziliaze all button.
    */
   @FXML
   public void initialize() {
+
     PayCostumerController menuController = new PayCostumerController(this);
+    totalBillToPayField.setOnAction(event -> menuController.handlePrize());
+    submitPaymentBtn.setOnAction(event -> menuController.handlePaymentSubmission());
     signOutBtn.setOnAction(event -> handleSignOut());
     menuBtn.setOnAction(event -> handleMenuCostumer());
+    clearFieldBtn.setOnAction(event -> handleClearFiled());
+    totalBillToPayField.setEditable(false);
+    totalBillToPayField.setMouseTransparent(true);
+    totalBillToPayField.setFocusTraversable(false);
+    tableNum.setText("Table Number: " + tableNumber);
+    menuController.handlePrize();
+  }
 
 
+
+  /**
+   * This method is used to get the first name of the costumer.
+   * 
+   * @return first name.
+   */
+  public String getFirstName() {
+
+    return firstNameField.getText();
+  }
+
+  /**
+   * This method is used to get the lasr name of the costumer.
+   * 
+   * @return last name.
+   */
+  public String getLastName() {
+
+    return lastNameField.getText();
+  }
+
+  /**
+   * This method is used to get the 16 digit number of the credit card.
+   * 
+   * @return the number of credit card.
+   */
+  public Long getNumberCard() {
+    try {
+      return Long.parseLong(cardNumberField.getText());
+    } catch (NumberFormatException e) {
+      return (long) -1;
+    }
+  }
+
+  /**
+   * The method is used to get the CCV number costumer card.
+   */
+  public Integer getCCV() {
+    try {
+      return Integer.parseInt(ccvField.getText());
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
+  /**
+   * The method is used to get the expaired date costumer card.
+   */
+  public String getExpairedDate() {
+
+    return expairedField.getText();
+  }
+
+
+  /**
+   * The method is used to get the expaired date costumer card.
+   */
+  public Integer getExpDate() {
+    String expDateStr = expairedField.getText(); // Assuming expairedField is accessible here
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yy");
+
+    try {
+      YearMonth expDate = YearMonth.parse(expDateStr, formatter);
+      YearMonth currentDate = YearMonth.now();
+
+      // Check if the expiration date is before the current date (meaning the card has expired)
+      if (expDate.isBefore(currentDate)) {
+        expairedField.setText("");
+        return -1; // Card is expired
+      } else {
+        return 1; // Card is valid
+      }
+    } catch (Exception e) {
+      // If parsing fails or any other error occurs, return -1 indicating an invalid or expired card
+      expairedField.setText("");
+      return -1;
+    }
+  }
+
+  /**
+   * This method is used to get the number of the table that will be use to to get the prise.
+   * 
+   * @return the number of table.
+   */
+  public Integer getTable() {
+    try {
+      return Integer.parseInt(tableNum.getText().replace("Table Number: ", ""));
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
+
+
+  /**
+   * This method is used to set the prize into text field.
+   * 
+   * @param bills is a total bills.
+   */
+  public void setPrize(Float bills) {
+
+    totalBillToPayField.setText("" + bills);
+  }
+
+
+  /**
+   * This method is used to clear all the field if the user change mind.
+   */
+  public void handleClearFiled() {
+    firstNameField.setText("");
+    cardNumberField.setText("");
+    lastNameField.setText("");
+    ccvField.setText("");
+    expairedField.setText("");
+  }
+
+  /**
+   * This method is used to clear all the field if the user change mind.
+   */
+  public void handleClearCCVFiled() {
+    ccvField.setText("");
+  }
+
+  /**
+   * This method is used to clear all the field if the user change mind.
+   */
+  public void handleClearCreditCardNumberFiled() {
+    cardNumberField.setText("");
   }
 
   /**
