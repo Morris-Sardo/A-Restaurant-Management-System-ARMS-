58d08abe9b120c0c49ccc9b29da6fa4e7b3c822d
Morris sardo <zlac176@live.rhul.ac.uk>
2024-03-22 21:26:47 +0000

    Fixing javadoc TitlePage enum class.

--- restaurant/src/main/java/appication/TitlePage.java
+++ restaurant/src/main/java/appication/TitlePage.java
@@ -7,9 +7,16 @@ package appication;
  *
  */
 public enum TitlePage {
-  LOGIN_PAGE("Login Page"), REVIEW_PAGE("Review Page Staff"), MENU_PAGE_COSTUMER(
-      "Menu Page Costumer"), INVENTROY_PAGE("Inventory Page"), MAKE_REVIEW_PAGE(
-          "Make Review Page"), MENU_PAGE("Menu Page"), DASHBOARD_PAGE(
+  /**
+   * This it the enum.
+   */
+  LOGIN_PAGE("Login Page"),
+  /**
+   * This is second.
+   */
+  REVIEW_PAGE("Review Page Staff"), MENU_PAGE_COSTUMER("Menu Page Costumer"), INVENTROY_PAGE(
+      "Inventory Page"), MAKE_REVIEW_PAGE("Make Review Page"), MENU_PAGE(
+          "Menu Page"), DASHBOARD_PAGE(
               "Dashboard Page"), KITCHEN_PAGE("Kitchen Page"), PRODUCT_ID_NUMBER_NO_VALID(
                   "Product ID number no valid"), PRIZE_NO_VALID("Prize no valid"), PAY_BILLS_PAGE(
                       "Pay Bills Page"), STOCK_NO_VALID("Stock no valid");
