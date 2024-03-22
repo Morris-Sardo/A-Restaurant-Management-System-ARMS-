6406405f73123fc186210f7d6b15f68f3389ca1a
jnthnmrtn04 <zlac181@live.rhul.ac.uk>
2024-03-22 10:15:27 +0000

    Create Model

---
+++ restaurant/src/main/java/appication/StockModel.java
@@ -0,0 +1,11 @@
+package appication;
+
+/**
+ * This class interacts with the stock database and GUI. When a user manipulates data on the
+ * GUI interface, it will be sent to the database.
+ * 
+ * @author jonathan
+ */
+public class StockModel {
+
+}
