067bc94ac2be99f1c5b61f316088cc745b5ae706
Zain Akhtar <ZLAC321@live.rhul.ac.uk>
2024-03-25 20:41:14 +0000

    Updated JavaDoc

--- restaurant/src/main/java/appication/MenuItem.java
+++ restaurant/src/main/java/appication/MenuItem.java
@@ -24,26 +24,56 @@ public class MenuItem {
     this.price = price;
   }
 
+  /**
+   * Getter for name of item.
+   * 
+   * @return item
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets the name of the item.
+   * 
+   * @param name Menu Item.
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Getter for the Quantity.
+   * 
+   * @return Quantity.
+   */
   public int getQuantity() {
     return quantity;
   }
 
+  /**
+   * Sets the quantity.
+   * 
+   * @param quantity Quantity for the item.
+   */
   public void setQuantity(int quantity) {
     this.quantity = quantity;
   }
 
+  /**
+   * Gets the price for the Menu.
+   * 
+   * @return Price.
+   */
   public double getPrice() {
     return price;
   }
 
+  /**
+   * Setter for the price.
+   * 
+   * @param price Price of the Menu Item.
+   */
   public void setPrice(double price) {
     this.price = price;
   }
