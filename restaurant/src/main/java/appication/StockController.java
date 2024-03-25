39b49ca1099f544a5a8181196d1105d13970c07f
jnthnmrtn04 <zlac181@live.rhul.ac.uk>
2024-03-25 21:08:23 +0000

    Clearing stock controller

--- restaurant/src/main/java/appication/StockController.java
+++ restaurant/src/main/java/appication/StockController.java
@@ -8,13 +8,6 @@ import javafx.scene.control.Alert.AlertType;
  * @author jonathan, micheal
  */
 public class StockController {
-  private KitchenView viewK;
-
-  public StockController(KitchenView viewK) {
-    this.viewK = viewK;
-
-
-  }
 
 }
 
